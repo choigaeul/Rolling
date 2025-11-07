32e9918685516c19a9aa148101cc9d64aecac43d
choigaeul <choigaeul@suwon.ac.kr>
2025-11-07 15:57:03 +0900

    feat.카드 컴포넌트 구현 50%

--- src/Card_list/Card_list_ver1.jsx
+++ src/Card_list/Card_list_ver1.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Card_list.css';
+import styles from './Card_list.module.css';
+import '../index.css';
 import profile01 from './assets/profile01.svg';
 import profile02 from './assets/profile02.svg';
 import profile03 from './assets/profile03.svg';
@@ -8,31 +9,32 @@ import pattern01 from './assets/pattern01.svg';
 import pattern03 from './assets/pattern03.svg';
 import pattern04 from './assets/pattern04.svg';*/
 
-
+/* 색깔 배경 */
 function CardList() {
-    return ( <>
-      <div className='card'>
-        <img className='cardDeco' src={pattern01} alt='pattern01'/>
-        <div className='cardHeader'>
-          <div className='toName'>To.Sowon</div>
-          <div className='cardProfile'>
-            <img src={profile01} alt='profile01'/>
-            <img src={profile02} alt='profile02'/>
-            <img src={profile03} alt='profile03'/> 
-            <span className='moreProfiles'>+27</span>
+  return (
+    <>
+      <div className={styles.card}>
+        <img className={styles.cardDeco} src={pattern01} alt='pattern01' />
+        <div className={styles.cardHeader}>
+          <div className={styles.toName}>To.Sowon</div>
+          <div className={styles.cardProfile}>
+            <img src={profile01} alt='profile01' />
+            <img src={profile02} alt='profile02' />
+            <img src={profile03} alt='profile03' />
+            <span className={styles.moreProfiles}>+27</span>
           </div>
-          <div className='writtenRecord'>
+          <div className={styles.writtenRecord}>
             <span>30명</span>이 작성했어요!
           </div>
         </div>
-        <div className='imojiWrapper'>
-          <div className='imoji'>👍 20</div>
-          <div className='imoji'>😍 12</div>
-          <div className='imoji'>😢 7</div>
+        <div className={styles.imojiWrapper}>
+          <div className={styles.imoji}>👍 20</div>
+          <div className={styles.imoji}>😍 12</div>
+          <div className={styles.imoji}>😢 7</div>
         </div>
       </div>
     </>
-    );
+  );
 }
 
 export default CardList
--- src/Card_list/Card_list_ver2.jsx
+++ src/Card_list/Card_list_ver2.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
-import './Card_list_ver2.css';
+import styles from './Card_list_ver2.module.css';
 import profile01 from './assets/profile01.svg';
 import profile02 from './assets/profile02.svg';
 import profile03 from './assets/profile03.svg';
 import bgImage from './assets/bgImg.avif';
 
-
+/* 이미지 배경 */
 function CardList2() {
     return ( <>
       <div
-      className="card"
+      className={styles.card}
       style={{
-        backgroundImage: `url(${bgImage})`, // 👈 배경 이미지 삽입
+        backgroundImage: `url(${bgImage})`,
       }}
       >
-        <div className='cardHeader'>
-          <div className='toName'>To.Sowon</div>
-          <div className='cardProfile'>
+        <div className={styles.cardHeader}>
+          <div className={styles.toName}>To.Sowon</div>
+          <div className={styles.cardProfile}>
             <img src={profile01} alt='profile01'/>
             <img src={profile02} alt='profile02'/>
             <img src={profile03} alt='profile03'/> 
-            <span className='moreProfiles'>+27</span>
+            <span className={styles.moreProfiles}>+27</span>
           </div>
-          <div className='writtenRecord'>
+          <div className={styles.writtenRecord}>
             <span>30명</span>이 작성했어요!
           </div>
         </div>
-        <div className='imojiWrapper'>
-          <div className='imoji'>👍 20</div>
-          <div className='imoji'>😍 12</div>
-          <div className='imoji'>😢 7</div>
+        <div className={styles.imojiWrapper}>
+          <div className={styles.imoji}>👍 20</div>
+          <div className={styles.imoji}>😍 12</div>
+          <div className={styles.imoji}>😢 7</div>
         </div>
       </div>
     </>
