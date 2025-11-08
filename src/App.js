c02d3bda57c7db19f68f750301be83436a0524e3
Oh Young gyo <oey157@nate.com>
2025-11-08 11:20:15 +0900

    merge

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import './App.css';
-<<<<<<< HEAD
-import Option from './Option/Option.jsx';
-import Header from './Header/Header.jsx'
-import MessageHeader from './Header/MessageHeader.jsx'
-import MobileHeader from './Header/MobileHeader.jsx'
-import User from './Option/User.jsx'
-=======
 import Card from './Card/Card.jsx';
 import AddCard from './Card/AddCard.jsx';
->>>>>>> Card
 
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Option/>
-      <Header/>
-      <MessageHeader/>
-      <MobileHeader/>
-      <User/>
-    </>
-=======
       <Card />
       <AddCard />
     </>
 
 
->>>>>>> Card
   );
 }
 
