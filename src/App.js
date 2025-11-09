146867c83b8c6aa9b5076b9e47ab7a6191575bf5
Oh Young gyo <oey157@nate.com>
2025-11-10 02:30:15 +0900

    appclear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "./App.css";
-import Header from "./Header/Header"
-import HeaderNobutton from "./Header/HeaderNobutton";
 
 function App() {
   return (
     <>
-      <HeaderNobutton/>
-      <Header/>
     </>
   );
 }
