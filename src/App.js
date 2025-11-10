5214358cd78923f4c15a43681403e8f0e89c99eb
choigaeul <choigaeul@suwon.ac.kr>
2025-11-11 01:06:59 +0900

    fix: app.js 정리 (진짜 찐 최종)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import React from 'react';
-import CreatePostPage from './CreatePostPage/CreatePostPage.jsx';
 
 function App() {
   return (
     <>
-      <CreatePostPage />
     </>
   );
 }
