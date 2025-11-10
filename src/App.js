10d717a4576ad32b3d355c34781090708d1e0d68
choigaeul <choigaeul@suwon.ac.kr>
2025-11-10 22:54:04 +0900

    feat: css 및 반응형 구현 완료 (기능 구현 전)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-<<<<<<< HEAD
 import './App.css';
-import Card from './Card/Card.jsx';
-import AddCard from './Card/AddCard.jsx';
-import CardList from './Card_list/Card_list_ver1.jsx';
-import CardList2 from './Card_list/Card_list_ver2.jsx';
-=======
-import "./App.css";
-import MessageHeader from "./Header/MessageHeader"
-import Header from "./Header/Header"
-import MobileHeader from "./Header/MobileHeader";
-
->>>>>>> 4fb51218b01081ef8b28f7ccf134dfe939d8471e
+import React from 'react';
+import CreatePostPage from './CreatePostPage/CreatePostPage.jsx';
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Card />
-      <AddCard />
-      <CardList />
-      <CardList2 />
-=======
-    <Header/>
-    <MessageHeader/>
-    <MobileHeader/>
->>>>>>> 4fb51218b01081ef8b28f7ccf134dfe939d8471e
+      <CreatePostPage />
     </>
   );
 }
